701ecf02573e97aa129581fad7cf125cce6aa6c2
Pero0123 <g00422221@atu.ie>
2023-11-30 14:35:00 +0000

    test code for servo

--- AutonomousVehicle/AutonomousVehicle.ino
+++ AutonomousVehicle/AutonomousVehicle.ino
@@ -1,41 +1,62 @@
-#include <ESP32Servo360.h>
+#include<Servo.h>
 
 //pins for front ultrasonic
 #define frontTrig 33
 #define frontEcho 34
-
+ 
 //pins for side ultrasonic
 #define sideTrig 25
 #define sideEcho 26
-
+ 
 //pins for servo
 #define continuesServo 27
 
+ 
 //motor pins
-#define RightMotorEnable 20
-#define RightMotorDir 21
-
-#define LeftMotorEnable 22
-#define LeftMotorDir 23
-
+#define RightMotorEnable 18
+#define RightMotorDir 19
+#define LeftMotorEnable 16
+#define LeftMotorDir 15
+ 
 #define UltrasonicOffset 40//physical offset of the ultrasonic in mm
-
+ 
 float distanceFront, distanceSide;
 int coords[2];
+ 
+Servo Servo1;
 
 void setup() {
   Serial.begin(115200);
-
 //sets pinmodes for all ultrasonic pins
   pinMode(frontTrig, OUTPUT);
   pinMode(sideTrig, OUTPUT);
   pinMode(frontEcho, INPUT);
   pinMode(sideEcho, INPUT);
 
+  pinMode(LeftMotorEnable, OUTPUT);
+  pinMode(RightMotorEnable, OUTPUT);
+  pinMode(RightMotorDir, OUTPUT);
+  pinMode(LeftMotorDir, OUTPUT);
+
+//create servo object for continues servo
+  Servo1.attach(continuesServo);
 }
 void loop() {
-
+ /*
   getDistance(0);
   getDistance(1);
-  
+  forwards(100);
+  delay(1000);
+  reverse(100);
+  delay(1000);
+  stop();
+  delay(1000);
+  */
+  //Servo1.write(90);
+  //delay(1000);
+  Servo1.write(92);
+  delay(1000);
+ // Servo1.write(0);
+ // delay(1000);
+ 
 }
--- AutonomousVehicle/Vehicle.ino
+++ AutonomousVehicle/Vehicle.ino
@@ -6,32 +6,34 @@
 //drive forwards at x speed
 void forwards(int speed)
 {
-  Serial.print("Forwards");
+  Serial.print("Forwards\n");
   analogWrite(RightMotorEnable,speed);
   analogWrite(LeftMotorEnable,speed);
 
   digitalWrite(RightMotorDir,LOW);
+  digitalWrite(LeftMotorDir,HIGH);
 }
 
 //reverse at x speed
 void reverse(int speed)
 {
-  Serial.print("Reverse");
+  Serial.print("Reverse\n");
   analogWrite(RightMotorEnable,speed);
   analogWrite(LeftMotorEnable,speed);
 
   digitalWrite(RightMotorDir,HIGH);
+  digitalWrite(LeftMotorDir,LOW);
 }
 
 //turn left
 //speed and radius can be controlled using speed variables
 void left(int speedRight ,int speedLeft)
 {
-  Serial.print("Left");
+  Serial.print("Left\n");
   analogWrite(RightMotorEnable,speedRight);
   analogWrite(LeftMotorEnable,speedLeft);
 
-  digitalWrite(LeftMotorDir,HIGH);//motor off
+  digitalWrite(LeftMotorDir,LOW);//motor off
   digitalWrite(RightMotorDir,LOW);//motor on
 }
 
@@ -39,12 +41,18 @@ void left(int speedRight ,int speedLeft)
 //speed and radius can be controlled using speed variables
 void right(int speedRight ,int speedLeft)
 {
-  Serial.print("Right");
+  Serial.print("Right\n");
   analogWrite(RightMotorEnable,speedRight);
   analogWrite(LeftMotorEnable,speedLeft);
 
-  digitalWrite(RightMotorDir,HIGH);//motor off
+  digitalWrite(RightMotorDir,LOW);//motor off
   digitalWrite(LeftMotorDir,LOW);//motor on 
+}
+
+void stop()
+{
+  digitalWrite(RightMotorEnable,LOW);//motor off
+  digitalWrite(LeftMotorEnable,LOW);//motor off 
 }
 
 
