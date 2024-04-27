5cccb8af2cdb4a09463d6154878e04ea0bbc08e3
Pero0123 <g00422221@atu.ie>
2024-04-27 22:36:13 +0100

    fixed operation of motors. Removed unused code. rewrote functions for compass

--- AutonomousVehicle/AutonomousVehicle.ino
+++ AutonomousVehicle/AutonomousVehicle.ino
@@ -1,82 +1,50 @@
 #include<Servo.h>
 #include <BluetoothSerial.h>
-//#include <QMC5883L.h>
 #include <Wire.h>
-#include "DFRobot_BMM150.h"
-#include <DFRobot_QMC5883.h>
+#include <QMC5883L.h>
 
 //pins for front ultrasonic
-#define frontTrig 32
-#define frontEcho 34
- 
-//pins for side ultrasonic
-#define sideTrig 33
-#define sideEcho 35
+#define Trig 32
+#define Echo 34
  
 //motor pins
-#define RightMotorEnable 18
-#define RightMotorDir 19
-#define LeftMotorEnable 16
-#define LeftMotorDir 15
+#define RightMotorEnable 26
+#define RightMotorDir 25
+#define LeftMotorEnable 33
+#define LeftMotorDir 32
 
-//pins for servo
-#define continuesServo 27
- 
-//physical offset of the ultrasonic in mm
-#define UltrasonicOffsetSide 40
-#define UltrasonicOffsetFront 40
- 
-// variables to store distance from ultrasonics
-float distanceFront, distanceSide;
-float coords[2] = {1,1};//stores co-ordinates of the vehicle
-int vehicleHeading = 999;//stores vehicle heading from compass1
-int platformHeading = 999;//stores ultrasonic heading from compass2
+
+int vehicleHeading = 999;//stores vehicle heading from compass
 int targetHeading = 140;//the heading the vehicle should drive towards
 int maxHeading, minHeading;
 int headingRange = 12;
 
-int targetPlatformHeading = 80;//the heading the ultrasonic platform should point towards
-int maxPlatformHeading, minPlatformHeading;
-int headingRangePlatform = 2;
-
+//bluetooth stuff here
 char BTmessage;
 
-//bmm150 compass definitions
-DFRobot_BMM150_I2C bmm150(&Wire, I2C_ADDRESS_4);
-DFRobot_QMC5883 compass(&Wire, /*I2C addr*/0x0d);
-
-//creates objects for sensors
-Servo Servo1;
+//sensor objects
+Servo UltrasonicServo;
 BluetoothSerial SerialBT;
-//QMC5883L compass1;
+QMC5883L compass;
 
 void setup() {
   Serial.begin(115200);
-  bluetoothSetup();//setup code for bluetooth
-  ultrasonicSetup();//setup code for the ultrasonic sensor on the tracking platform
-  Servo1.attach(continuesServo);//create servo object for continues servo
-  //compassSetup();
-  bmm155Setup();//setup code for the bmm150 compass module
-  //qmcSetup();
+  //bluetoothSetup();//setup code for bluetooth
+  compassSetup();
+
+  //motor pins setup
+  pinMode(RightMotorDir,OUTPUT);
+  pinMode(RightMotorEnable,OUTPUT);
+  pinMode(LeftMotorDir,OUTPUT);
+  pinMode(LeftMotorEnable,OUTPUT);
 }
 
 void loop() {
  
-  //demo vehicle driving towards a heading set via bluetooth
-  vehicleHeading = bmm150.getCompassDegree();
-  bluetoothHeadingControl();
-  printCoordsToBluetooth();
-  configureVehicleHeading();
-  driveTowardHeading();
-  delay(100);
-
+  // //Vehicle driving towards a set heading
+  // vehicleHeading = getHeading();
+  // configureVehicleHeading();
+  // driveTowardHeading();
+  // delay(50);
  
-  //bluetooth control and ultrasonic co-ordinates and stailisation demo.
-  //platformHeading = getDFCompasData(); //reads current heading
-  bluetoothControlRead(); //controls motors using bluetooth
-  getCoords(); //gets co-ordinates from ultrasonics //adds in a 100ms delay
-  correctPlatformHeading();
-  printCoordsToBluetooth();//prints out information about the vehicle to bluetooth serial monitor
-  delay(30);
-  
 }
--- AutonomousVehicle/Compass.ino
+++ AutonomousVehicle/Compass.ino
@@ -1,80 +1,24 @@
-/*
-void compassSetup()
+void compassSetup()//setup code for the gy-271 magnetometer
 {
-  Wire.begin();
-	compass1.init();
-	compass1.setSamplingRate(120);
+	Wire.begin();
+
+	compass.init();
+	compass.setSamplingRate(50);
+
 	Serial.begin(115200);
 	Serial.println("QMC5883L Compass Demo");
 	Serial.println("Turn compass in all directions to calibrate....");
-}
-*/
-void qmcSetup()
-{
-  while (!compass.begin())
-  {
-    Serial.println("Could not find a valid 5883 sensor, check wiring!");
-    delay(500);
-  }
-  if(compass.isQMC())
-  {
-    Serial.println("Initialize QMC5883");
-    // compass.setRange(QMC5883_RANGE_2GA);
-    // Serial.print("compass range is:");
-    // Serial.println(compass.getRange());
-
-     compass.setMeasurementMode(QMC5883_CONTINOUS);
-     Serial.print("compass measurement mode is:");
-     Serial.println(compass.getMeasurementMode());
-
-    compass.setDataRate(QMC5883_DATARATE_50HZ);
-    Serial.print("compass data rate is:");
-    Serial.println(compass.getDataRate());
-
-    compass.setSamples(QMC5883_SAMPLES_8);
-    Serial.print("compass samples is:");
-    Serial.println(compass.getSamples());
-  }
-  delay(1000);
+  compass.resetCalibration();
 }
 
-
-float getDFCompasData()
+int getHeading()//returns the heading of the compass. includes auto calibration
 {
-  float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
-  compass.setDeclinationAngle(declinationAngle);
-  sVector_t mag = compass.readRaw();
-  compass.getHeadingDegrees();
-  Serial.print("Degress = ");
-  Serial.println(mag.HeadingDegress);
-  return mag.HeadingDegress;
-}
-
-void bmm155Setup()
-{
-  //bmm150 compass setup code
-
-  while(!Serial);
-  while(bmm150.begin()){
-    Serial.println("bmm150 init failed, Please try again!");
-    delay(1000);
-  } Serial.println("bmm150 init success!");
-  bmm150.setOperationMode(BMM150_POWERMODE_NORMAL);
-  bmm150.setPresetMode(BMM150_PRESETMODE_HIGHACCURACY);
-  bmm150.setRate(BMM150_DATA_RATE_20HZ);
-  bmm150.setMeasurementXYZ();
-}
-
-/*
-//gets heading from the gy-271 compass module
-int getCompassHeading()
-{
-	int heading = compass1.readHeading();
-	if(heading==0) {
-
+	int h = compass.readHeading();
+	if(h==0) {
+		/* Still calibrating, so measure but don't print */
+    Serial.println("compass is calibrating");
 	} else {
-		Serial.println(heading);
+		Serial.println(h);
+    return h;
 	}
-  return heading;
 }
-*/
